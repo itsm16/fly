c4a20190cd3e5e7df6473d743245982233b93c7e
itsm16 <mrinalg2k1@gmail.com>
2025-03-25 21:29:15 +0530

    nav

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 function Login() {
   const navigate = useNavigate();
